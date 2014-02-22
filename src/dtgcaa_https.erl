7d77442d540b36ec35bc145642ff0113f9dcd0c4
Klaus Trainer <klaus_trainer@posteo.de>
2014-02-22 04:08:09 +0100

    Remove obsolete ssl workaround

--- src/dtgcaa_https.erl
+++ src/dtgcaa_https.erl
@@ -19,15 +19,11 @@ start(Port) ->
             {"/:uri", dtgcaa_handler, []}
         ]}
     ]),
-    UnBrokenCiphers = lists:filter(fun(Suite) ->
-        string:left(atom_to_list(element(1, Suite)), 4) =/= "ecdh"
-    end, ssl:cipher_suites()),
     cowboy:start_https(?MODULE, 64, [
         {port, Port},
         {cacertfile, PrivDir ++ "/ssl/ca.crt"},
         {certfile, PrivDir ++ "/ssl/server.crt"},
-        {keyfile, PrivDir ++ "/ssl/server.key"},
-        {ciphers, UnBrokenCiphers}
+        {keyfile, PrivDir ++ "/ssl/server.key"}
     ], [{env, [{dispatch, Dispatch}]}]).
 
 stop(Pid) ->
